ac14dcd907fffc49df554294cee16f2adda09908
dansuzuki <kernelonsteroids@gmail.com>
2016-11-13 07:23:22 -0500

    added src/main/resources/log4j.properties for default log4j appender added log4j on pom.xml fixed WordCount.java

--- src/main/java/com/talas/examples/WordCount.java
+++ src/main/java/com/talas/examples/WordCount.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -12,12 +13,19 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+
+import org.apache.log4j.*;
+
+
 public class WordCount extends Configured implements Tool {
+
+
 
   public static class Map
        extends Mapper<LongWritable, Text, Text, IntWritable>{
@@ -25,9 +33,7 @@ public class WordCount extends Configured implements Tool {
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
 
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
-
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
       for(String token : value.toString().split("\\s+")) {
           word.set(token);
           context.write(word, one);
@@ -39,9 +45,7 @@ public class WordCount extends Configured implements Tool {
        extends Reducer<Text,IntWritable,Text,IntWritable> {
     private IntWritable result = new IntWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
       int sum = 0;
       for (IntWritable val : values) {
         sum += val.get();
@@ -53,18 +57,26 @@ public class WordCount extends Configured implements Tool {
 
   @Override
   public int run(String[] args) throws Exception {
+    // just to make configuration readable
+    PropertyConfigurator.configure(getClass().getClassLoader().getResourceAsStream("log4j.properties"));
+    
     Configuration conf = getConf();
     Job job = Job.getInstance(conf, "word count");
     job.setJarByClass(WordCount.class);
     job.setMapperClass(Map.class);
     job.setReducerClass(Reduce.class);
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(IntWritable.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     return job.waitForCompletion(true) ? 0 : 1;
   }
   public static void main(String[] args) throws Exception {
+
     int res = ToolRunner.run(new Configuration(), new WordCount(), args);
     System.exit(res);
   }
